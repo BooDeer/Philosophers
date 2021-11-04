ff8019a348f70a3c069125335b20d7275f3371fa
BooDeer <hsissi3a@gmail.com>
2021-11-04 12:38:39 +0100

    added the philo struct and mutexes

--- philo.h
+++ philo.h
@@ -14,21 +14,31 @@
 # define COLORS_RED "\033[031m"
 
 
-typedef	struct s_philo
+typedef	struct s_data
 {
 	int	philo_nb;
 	int	t_td;
 	int	t_te;
 	int	t_ts;
 	int	rds;
+	pthread_mutex_t *forks;
+	pthread_mutex_t quil;
+}			t_data;
+
+typedef struct s_philo
+{
+	int	id;
+	int	l_ate;
+	pthread_t p;
+	t_data *data;
 }			t_philo;
 
 int	error_message(char *error_str, int error_code);
-void	print_philo(t_philo data);
+void	print_philo(t_data data);
 int		ft_isdigit(int c);
 int		ft_atoi(const char *c);
 int		check_data(char **av);
-t_philo fill_data(char **data, int data_nb);
+t_data *fill_data(char **data, int data_nb);
 
 
 #endif
